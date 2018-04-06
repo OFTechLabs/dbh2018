58b43ef2eb36ab101e875dcf05f28006a826b29a
MikeM <mike.markus@ortec-finance.com>
2018-04-06 19:41:09 +0200

    Wired up some @Input's

--- frontend/src/app/components/feasibility/feasibility.component.ts
+++ frontend/src/app/components/feasibility/feasibility.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-feasibility',
@@ -7,6 +7,8 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FeasibilityComponent implements OnInit {
+  @Input() feasibilityPercentage;
+
   constructor() {}
 
   ngOnInit() {}
