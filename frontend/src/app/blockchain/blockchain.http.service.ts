ef107b0b3de1e8005ad915627271c46ab7ec5494
jelleverstegen <jelle.verstegen@gmail.com>
2018-04-07 09:46:54 +0200

    can now succesfully call ethereum

--- frontend/src/app/blockchain/blockchain.http.service.ts
+++ frontend/src/app/blockchain/blockchain.http.service.ts
@@ -2,13 +2,17 @@ import * as Web3 from 'web3';
 import { abiArray } from './abi';
 
 export class BlockchainHttpService {
-  public static provider = new Web3.providers.HttpProvider('http://ec2-18-184-38-165.eu-central-1.compute.amazonaws.com:8545/');
-  public static web3 = new Web3(BlockchainHttpService.provider);
-
+  public static web3 = new Web3(new Web3.providers.HttpProvider('http://ec2-18-184-38-165.eu-central-1.compute.amazonaws.com:8545/'));
   public static contractInstance = BlockchainHttpService.web3.eth.contract(abiArray).at('0x731a10897d267e19B34503aD902d0A29173Ba4B1');
 
+  public static getContractInstance() {
+    if (BlockchainHttpService.web3.eth.defaultAccount === null || BlockchainHttpService.web3.eth.defaultAccount === undefined) {
+      BlockchainHttpService.web3.eth.defaultAccount = '0x00a329c0648769A73afAc7F9381E08FB43dBEA72';
+    }
+    return BlockchainHttpService.contractInstance;
+  }
+
   public static async getYear() {
-    BlockchainHttpService.web3.eth.defaultAccount = '4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7';
-    return BlockchainHttpService.contractInstance.getYear();
+    return await BlockchainHttpService.getContractInstance().getYear([1]);
   }
 }
